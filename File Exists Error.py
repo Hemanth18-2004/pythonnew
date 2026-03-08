637ac2aaaf71768c88ec4c731f1ecc521d5758db
Hemanth D C <hemanth.d.c.9686@gmail.com>
2026-03-08 11:44:16 +0530

    Delete File Exists Error.py

--- File Exists Error.py
+++
@@ -1,5 +0,0 @@
-def create_file(filepath):
-    open(filepath, 'x') # creates a file if it does not exists
-    print(f'{filepath} created successfully')
-
-create_file('new_file1.txt') # FileExistsError: [Errno 17] File exists: 'new_file1.txt'
