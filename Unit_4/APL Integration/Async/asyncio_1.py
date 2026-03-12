4ff532185c203b6f0d6abef9a3a53535df1345d9
Hemanth D C <hemanth.d.c.9686@gmail.com>
2026-03-12 19:52:30 +0530

    Update asyncio_1.py

--- Unit_4/APL Integration/Async/asyncio_1.py
+++ Unit_4/APL Integration/Async/asyncio_1.py
@@ -1,2 +1,11 @@
 import asyncio
 import time
+''' Define a coroutine'''
+async def Task(name, delay):
+    print(f"Task {name} started execution at {time.strftime('%H:%M:%S')}")
+    await asyncio.sleep(10)
+    ''' Coroutine is paused for 10 seconds
+        Control is returned to the event loop during the delay'''
+    end_t = time.strftime('%H:%M:%S')
+    print(f'Task {name} completed execution at {end_t}')
+    return name, end_t
