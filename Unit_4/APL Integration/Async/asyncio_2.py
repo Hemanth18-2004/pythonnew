eba8edbc7623983a20b0f35d81331b38fcbb5034
Hemanth D C <hemanth.d.c.9686@gmail.com>
2026-03-12 19:56:36 +0530

    Update asyncio_2.py

--- Unit_4/APL Integration/Async/asyncio_2.py
+++ Unit_4/APL Integration/Async/asyncio_2.py
@@ -9,3 +9,14 @@ async def Job(name, delay):
     tt = round(end-start, 2)
     print(f'Job {name} executed in {tt} seconds')
     return name, tt
+async def main():
+    t0 = time.time()
+    ''' Overall start time '''
+    results = await asyncio.gather(Job(1, 12), Job(2, 3), Job(3, 1), Job(4, 2), Job(5, 5), Job(6, 2))
+    t1 = time.time()
+    print(f'Overall time taken for execution is {t1 - t0} seconds')
+    print('Individual Task Time')
+    for name, taken in results:
+        print(f'Job {name} took {taken} seconds')
+
+asyncio.run(main())
