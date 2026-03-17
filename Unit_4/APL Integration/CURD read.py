fe6794e5ddb393e3c9bf9f3807e90d7f4772d559
Hemanth D C <hemanth.d.c.9686@gmail.com>
2026-03-17 22:28:11 +0530

    Update CURD read.py

--- Unit_4/APL Integration/CURD read.py
+++ Unit_4/APL Integration/CURD read.py
@@ -8,3 +8,18 @@ conn = psy.connect(
     host = 'localhost',
     port = '5432'
 )
+
+if conn:
+    print('Connection established successfully.')
+else:
+    print('Connection to PostgreSQL encountered error.')
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM students_1")
+rows = cur.fetchall()
+conn.commit()
+cur.close()
+conn.close()
+
+for row in rows:
+    print(row)
